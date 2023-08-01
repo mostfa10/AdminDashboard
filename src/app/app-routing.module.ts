601f9673935da58079877b5ad08c715a5c0ac990
Safouan <safwantmimi.st@gmail.com>
2023-08-01 20:13:12 +0200

    correctifs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { ProfilComponent } from './components/profil/profil.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  {path:"",component:HomeComponent},
   {path:"home",component:HomeComponent,children:[
     {path:"profile",component:ProfilComponent},
     {path:"listO",component:ListOffreComponent},
